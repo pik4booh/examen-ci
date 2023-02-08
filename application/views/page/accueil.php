062a0cf73851c50e54200534d34f5cb4efb4ecd4
pik4booh <124702206+koloinaAP@users.noreply.github.com>
2023-02-08 14:52:47 +0300

    accueil

--- application/views/page/accueil.php
+++ application/views/page/accueil.php
@@ -1,94 +1,25 @@
 
     <p style="height: 75px;margin: 50px;font-size: 37px;letter-spacing: 1px;font-weight: bold;">LISTE PRODUIT</p><!-- Start: Masonry gallery cards responsive -->
     <div>
-        
         <!-- Start: cards -->
         <div class="row" data-masonry="{&quot;percentPosition&quot;: true }">
+        <?php foreach ($listOtherObjet as $loo) {?>
+
             <!-- Start: card-1 -->
             <div class="col-sm-6 col-lg-4 mb-4">
                 <!-- Start: card -->
                 <div class="card">
-                    <picture type="" srcset=""><img class="card-img-top p-3" src="assets/img/p-1.jpg" alt="" style="border-radius: 24px; object-fit: cover"></picture><!-- Start: texto -->
+                    <picture type="" srcset=""><img class="card-img-top p-3" src="<?php echo base_url(); ?>/assets/img/<?php echo $loo['cover']; ?>" alt="" style="border-radius: 24px; object-fit: cover"></picture><!-- Start: texto -->
                     <div class="card-body">
-                        <h5 class="card-title">Titulo<br></h5>
-                        <p class="card-text text-muted">Parrafo<br></p><a href="">Link</a>
+                        <h4 class="card-title"><?php echo $loo['nom']; ?><br></h4>
+                        <h6><?php echo $loo['userName']; ?></h6>
+                        <p class="card-text text-muted"><?php echo $loo['descri']; ?><br></p><a href="">Link</a>
                     </div><!-- End: texto -->
                 </div><!-- End: card -->
             </div><!-- End: card-1 -->
 
-            <!-- Start: card-2 -->
-            <div class="col-sm-6 col-lg-4 mb-4">
-                <!-- Start: card -->
-                <div class="card">
-                    <picture type="" srcset=""><img class="card-img-top p-3" src="assets/img/p-2.jpg" alt="" style="border-radius: 24px; object-fit: cover"></picture><!-- Start: texto -->
-                    <div class="card-body">
-                        <h5 class="card-title">Titulo<br></h5>
-                        <p class="card-text text-muted">Parrafo<br></p><a href="">Link</a>
-                    </div><!-- End: texto -->
-                </div><!-- End: card -->
-            </div><!-- End: card-2 -->
-            <!-- Start: card-3 -->
-            <div class="col-sm-6 col-lg-4 mb-4">
-                <!-- Start: card -->
-                <div class="card"><img class="card-img-top p-3" src="assets/img/p-3.jpg" alt="" style="border-radius: 24px; object-fit: cover"><!-- Start: texto -->
-                    <div class="card-body">
-                        <h5 class="card-title">Titulo<br></h5>
-                        <p class="card-text text-muted">Parrafo<br></p><a href="">Link</a>
-                    </div><!-- End: texto -->
-                </div><!-- End: card -->
-            </div><!-- End: card-3 -->
-            <!-- Start: card-4 -->
-            <div class="col-sm-6 col-lg-4 mb-4">
-                <!-- Start: card -->
-                <div class="card">
-                    <picture type="" srcset=""><img class="card-img-top p-3" src="assets/img/p-4.jpg" alt="" style="border-radius: 24px; object-fit: cover"></picture><!-- Start: texto -->
-                    <div class="card-body">
-                        <h5 class="card-title">Titulo<br></h5>
-                        <p class="card-text text-muted">Parrafo<br></p><a href="">Link</a>
-                    </div><!-- End: texto -->
-                </div><!-- End: card -->
-            </div><!-- End: card-4 -->
-            <!-- Start: card-5 -->
-            <div class="col-sm-6 col-lg-4 mb-4">
-                <!-- Start: card -->
-                <div class="card">
-                    <picture type="" srcset=""><img class="card-img-top p-3" src="assets/img/p-5.jpg" alt="" style="border-radius: 24px; object-fit: cover"></picture><!-- Start: texto -->
-                    <div class="card-body">
-                        <h5 class="card-title">Titulo<br></h5>
-                        <p class="card-text text-muted">Parrafo<br></p><a href="">Link</a>
-                    </div><!-- End: texto -->
-                </div><!-- End: card -->
-            </div><!-- End: card-5 -->
-            <!-- Start: card-6 -->
-            <div class="col-sm-6 col-lg-4 mb-4">
-                <!-- Start: card -->
-                <div class="card"><img class="card-img-top p-3" src="assets/img/p-6.jpg" alt="" style="border-radius: 24px; object-fit: cover"><!-- Start: texto -->
-                    <div class="card-body">
-                        <h5 class="card-title">Titulo<br></h5>
-                        <p class="card-text text-muted">Parrafo<br></p><a href="">Link</a>
-                    </div><!-- End: texto -->
-                </div><!-- End: card -->
-            </div><!-- End: card-6 -->
-            <!-- Start: card-7 -->
-            <div class="col-sm-6 col-lg-4 mb-4">
-                <!-- Start: card -->
-                <div class="card">
-                    <picture type="" srcset=""><img class="card-img-top p-3" src="assets/img/p-7.jpg" alt="" style="border-radius: 24px; object-fit: cover"></picture><!-- Start: texto -->
-                    <div class="card-body">
-                        <h5 class="card-title">Titulo<br></h5>
-                        <p class="card-text text-muted">Parrafo<br></p><a href="">Link</a>
-                    </div><!-- End: texto -->
-                </div><!-- End: card -->
-            </div><!-- End: card-7 -->
-            <!-- Start: card-8 -->
-            <div class="col-sm-6 col-lg-4 mb-4">
-                <!-- Start: card -->
-                <div class="card"><img class="card-img-top p-3" src="assets/img/p-8.jpg" alt="" style="border-radius: 24px; object-fit: cover"><!-- Start: texto -->
-                    <div class="card-body">
-                        <h5 class="card-title">Titulo<br></h5>
-                        <p class="card-text text-muted">Parrafo<br></p><a href="">Link</a>
-                    </div><!-- End: texto -->
-                </div><!-- End: card -->
-            </div><!-- End: card-8 -->
+        <?php } ?>
+        </div>
+        </div>
             <!-- Start: Footer with social media icons -->
             
