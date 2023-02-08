ce3cc7814839cecea6120dbccb0500f282a1467b
pik4booh <124702206+koloinaAP@users.noreply.github.com>
2023-02-08 14:19:25 +0300

    cool

--- application/controllers/Client.php
+++ application/controllers/Client.php
@@ -5,7 +5,9 @@ class Client extends CI_Controller {
     public function index()
 	{
         // $data['content'] = 'Client';
-		// $this->load->view('page/accueil', $data);
+		// $data['listeMarque'] = $this->Model->listeMarque();
+
+		$data^['listOtherObj'] = $this->Objet->getOtherObjets();
 		$data['contents'] = 'page/accueil';
 		$this->load->view('accueil',$data);
 	}	
